d8445ded24afbb54099b982ffeab17a008d7dbc8
Jacob <jacobel@kth.se>
2020-11-26 20:06:16 +0100

    size and details on piechart

--- src/Components/SunburstComponent.jsx
+++ src/Components/SunburstComponent.jsx
@@ -1,22 +1,25 @@
 import { useD3 } from '../hooks/useD3';
 import React, { component, useRef, useEffect } from 'react';
 import * as d3 from 'd3';
+import categories from '../Data/tasks'
 
-const SunburstComponent = ({data}) => {
-	const width = 450;
-    const height = 450;
-    const margin = { top: 40, right: 60, bottom: 60, left: 80 };
+const transitionDuration = 900;
+
+const SunburstComponent = ({widthHeightValue = 300, data}) => {
+
+	const margin = { top: 50, right: 0, bottom: 0, left: 0 };
+	const width = widthHeightValue - margin.left - margin.right - margin.top;
+    const height = widthHeightValue - margin.top - margin.bottom;
 
     let d3Container = useRef(null);
 
     useEffect (( ) => {
     	    if(data && d3Container.current) {
     	    	data = Array.from(data, ([key, value]) => ({key,value}));
-    	    	console.log(data);
 
         		const radius = Math.min(width, height) / 2 ;
 
-        		console.log(radius);
+				d3.select(d3Container.current).selectAll('*').remove();
 		    	const svg = d3.select(d3Container.current)
 			  			.append("svg")
 			    		.attr("width", width)
@@ -26,7 +29,7 @@ const SunburstComponent = ({data}) => {
 
 			    const color = d3.scaleOrdinal()
 		  						.domain(data.map(d => d.key))
-		  						.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
+		  						.range(["#204051", "#3B5070", "#675C88", "#996694", "#C97190","#ED8382","#FFA06F"])
 
 		  		const pie = d3.pie().value(d => d.value)
 
@@ -40,7 +43,20 @@ const SunburstComponent = ({data}) => {
 		                .data(arcData)
 		                .join("path")
 		                .attr("fill", d=>color(d.key))
+		                .attr("stroke", "white")
 		                .attr("d",arc);
+
+		        const newarc = d3.arc()
+		        				.innerRadius(2 * radius / 3)
+		        				.outerRadius(radius);
+
+		        arcs.append("text")
+		        	.attr("transform", d => {
+		        		return "translate(" + newarc.centroid(d) + ")";
+		        	})
+		        	.attr("text-anchor", "middle")
+		        	.attr("fill", "white")
+		        	.text(d => d.key);
     		}
 
 
